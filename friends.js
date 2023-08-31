bb36c44d477dca34a791a1f5bfa9ec87e4f782b5
Eric Batiste <ericbatiste4@gmail.com>
2023-08-31 09:39:23 -0600

    Array method

--- friends.js
+++ friends.js
@@ -4,3 +4,6 @@ console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
 
+var array2 = ["Positano", "Reykjavik", "Cape Town", "Batumi", "Christ Church"].reverse();
+console.log(array2);
+
